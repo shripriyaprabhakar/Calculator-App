d2f6f9b56b7b04c9daecae41a9769abfd90961b2
priya suresh <priyasuresh@MacBook-Pro.local>
2018-08-19 18:46:45 -0700

    added basics for new app

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    };
+  }
+
+  render() {
+    return (
+      <div className="Calc-container">
+        Calculator 
+      </div>
+    );
+  }
+}
---
+++ client/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import App from './App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
